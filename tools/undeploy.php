1d862e5aebd465fe64ed5269bd70fee97bdd08fa
slavination <151602883+slavination@users.noreply.github.com>
2024-10-03 12:07:44 +0700

    Update undeploy.php

--- tools/undeploy.php
+++ tools/undeploy.php
@@ -1,20 +1,12 @@
 <?php 
-// PHP program to delete all 
-// file from a folder 
 
-// Folder path to be flushed 
 $folder_path = "data.txt"; 
 
-// List of name of files inside 
-// specified folder 
 $files = glob($folder_path.'/*'); 
 
-// Deleting all the files in the list 
 foreach($files as $file) { 
 
 	if(is_file($file)) 
-	
-		// Delete the given file 
 		rmdir($file); 
 } 
 ?>
