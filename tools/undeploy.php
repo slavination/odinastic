4a39080edfccf1e4746fa1e4fb05ad70bc5f87d3
slavination <151602883+slavination@users.noreply.github.com>
2024-10-03 12:16:18 +0700

    Update undeploy.php

--- tools/undeploy.php
+++ tools/undeploy.php
@@ -1,20 +1,23 @@
-<?php 
-// PHP program to delete all 
-// file from a folder 
+<?php
 
-// Folder path to be flushed 
-$folder_path = "data.txt"; 
+// Nama file yang berisi daftar folder yang akan dihapus
+$file = 'data.txt';
 
-// List of name of files inside 
-// specified folder 
-$files = glob($folder_path.'/*'); 
+// Membaca isi file dan mengonversinya menjadi array
+$folders = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// Deleting all the files in the list 
-foreach($files as $file) { 
+// Menghapus setiap folder yang terdaftar
+foreach ($folders as $folder) {
+    // Memastikan folder ada dan merupakan direktori
+    if (is_dir($folder)) {
+        // Menghapus folder
+        rmdir($folder);
+        echo "Folder \"$folder\" berhasil dihapus.<br>";
+    } else {
+        echo "Folder \"$folder\" tidak ditemukan atau bukan folder.<br>";
+    }
+}
 
-	if(is_file($file)) 
-	
-		// Delete the given file 
-		rmdir($file); 
-} 
-?> 
+echo 'Proses penghapusan selesai.';
+
+?>
