491adb021ef6231a30e422afbc540bbd7c814471
slavination <151602883+slavination@users.noreply.github.com>
2024-10-03 11:57:55 +0700

    Update undeploy.php

--- tools/undeploy.php
+++ tools/undeploy.php
@@ -1,20 +1,5 @@
 <?php
 $hostname = 'pramukalia.com';
-$dataFolder = 'data/';
-$templateFolder = 'templates/';
-
-echo "Memeriksa folder data: $dataFolder\n";
-
-// Array untuk menyimpan URL untuk sitemap
-$sitemapUrls = [];
-
-// Fungsi untuk membaca template PHP dari folder templates
-
-
-// Memuat template PHP dari folder templates
-$htmlTemplates = loadTemplates($templateFolder);
-
-// Membaca file data.txt untuk mendapatkan daftar keyword
 $filePath = 'data.txt';
 $dirname = '$filePath';
 // Array untuk menyimpan tautan internal
